57b82a5ff67f12f5bcabd5ab0b8ead954fd20475
sattya19 <sattyasharma195@gmail.com>
2023-10-02 13:52:39 +0530

    added heapSort algo

---
+++ 2 . Sorting/heapSort.java
@@ -0,0 +1,66 @@
+public class HeapSort {
+    public static void main(String[] args) {
+        int[] arr = { 12, 11, 13, 5, 6, 7 };
+
+        System.out.println("Original Array:");
+        printArray(arr);
+
+        heapSort(arr);
+
+        System.out.println("\nSorted Array:");
+        printArray(arr);
+    }
+
+    public static void heapSort(int[] arr) {
+        int n = arr.length;
+
+        // Build a max heap
+        for (int i = n / 2 - 1; i >= 0; i--) {
+            heapify(arr, n, i);
+        }
+
+        // Extract elements from the heap one by one
+        for (int i = n - 1; i >= 0; i--) {
+            // Move the current root to the end
+            int temp = arr[0];
+            arr[0] = arr[i];
+            arr[i] = temp;
+
+            // Call max heapify on the reduced heap
+            heapify(arr, i, 0);
+        }
+    }
+
+    public static void heapify(int[] arr, int n, int i) {
+        int largest = i;
+        int left = 2 * i + 1;
+        int right = 2 * i + 2;
+
+        // If left child is larger than the root
+        if (left < n && arr[left] > arr[largest]) {
+            largest = left;
+        }
+
+        // If right child is larger than the largest so far
+        if (right < n && arr[right] > arr[largest]) {
+            largest = right;
+        }
+
+        // If the largest element is not the root
+        if (largest != i) {
+            int swap = arr[i];
+            arr[i] = arr[largest];
+            arr[largest] = swap;
+
+            // Recursively heapify the affected sub-tree
+            heapify(arr, n, largest);
+        }
+    }
+
+    public static void printArray(int[] arr) {
+        for (int value : arr) {
+            System.out.print(value + " ");
+        }
+        System.out.println();
+    }
+}
